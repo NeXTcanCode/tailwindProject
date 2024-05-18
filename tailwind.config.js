bae9cbbf0d6bf1c8cfe6ffba2657615f6f3d9e8f
Vikas Sinha <vikassinha@Vikass-MacBook-Air.local>
2024-05-18 08:46:44 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        cursive: '"Cedarville Cursive", cursive',
+        sedan: '"Sedan SC",serifs',
+      },
+    },
+  },
+  plugins: [],
+};
